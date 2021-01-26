ac13ed50d061222acf573904deef4a81111cdd2a
Alexander Humeniuk <alexander.humeniuk@gmail.com>
2021-01-26 20:13:35 +0100

    GPU or CUDA?

--- semiclassical/cli.py
+++ semiclassical/cli.py
@@ -91,16 +91,6 @@ def main():
                              help='do not open window for plotting, only export tables.')
     
     args = parser.parse_args()
-
-    # # GPU or CPU ?
-    global device
-    torch.set_default_dtype(torch.float64)
-    if torch.cuda.is_available():
-        args.cuda = min(args.cuda, torch.cuda.device_count())
-        device = torch.device(f"cuda:{args.cuda}")
-    else:
-        device = torch.device('cpu')
-    logger.info(f"running on {device}")
     
     try:
         
@@ -125,6 +115,16 @@ class ConfigurationError(Exception):
         
 def _run_semiclassical_dynamics(task):
     assert task['title'] == "internal conversion"
+
+    # run on GPU or CPU ?
+    global device
+    torch.set_default_dtype(torch.float64)
+    if torch.cuda.is_available():
+        args.cuda = min(args.cuda, torch.cuda.device_count())
+        device = torch.device(f"cuda:{args.cuda}")
+    else:
+        device = torch.device('cpu')
+    logger.info(f"running on {device}")
     
     # ground state potential and non-adiabatic couplings
     p = task['potential']
